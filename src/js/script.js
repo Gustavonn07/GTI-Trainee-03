af451520c9317afcc4bf27c0a50b662a3f44c9fd
Gustavonn07 <gustavonepnog@gmail.com>
2023-11-15 22:01:33 -0300

    add: começando a criar os cards

--- src/js/script.js
+++ src/js/script.js
@@ -6,11 +6,30 @@ async function renderizaPokemon() {
     try {
         const pokemons = await gerarPokemon();
 
+        // Testes
+        const url = "https://assets.pokemon.com/assets/cms2/img/misc/countries/pt/country_detail_pokemon.png"
+
         root.innerHTML = pokemons.map(pokemon => {
             return pokemon.type2 ? `
-                <p class="text-red-500">${pokemon.nome}, ${pokemon.type}, ${pokemon.type2}</p> 
+                <div class="w-64 bg-slate-500 h-72 mx-auto mt-10 flex flex-col items-center gap-5">
+                    <img src=${url} alt="" class="w-full h-1/2">
+                    
+                    <h3 class="text-2xl">${pokemon.nome}</h3>
+                    <div class="flex gap-5">
+                        <h4>${pokemon.type}</h4>
+                        <h4>${pokemon.type2}</h4>
+                    </div> 
+                </div> 
+
             ` : `
-                <p class="text-red-500">${pokemon.nome}, ${pokemon.type}</p> 
+            <div class="w-64 bg-slate-500 h-72 mx-auto mt-10 flex flex-col items-center gap-5">
+            <img src=${url} alt="" class="w-full h-1/2">
+            
+            <h3 class="text-2xl">${pokemon.nome}</h3>
+            <div class="">
+                <h4>${pokemon.type}</h4>
+            </div> 
+        </div> 
             `;
         }).join('');
 
